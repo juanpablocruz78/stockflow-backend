8e00870f41e9efdd17f89e19093bc1ae8cd977b5
Juan Pablo Cruz Ulloa <cujp78@gmail.com>
2026-01-15 12:32:20 -0400

    initial Spring Boot project setup

---
+++ src/main/java/com/stockflow/StockflowBackendApplication.java
@@ -0,0 +1,13 @@
+package com.stockflow;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class StockflowBackendApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(StockflowBackendApplication.class, args);
+    }
+
+}
