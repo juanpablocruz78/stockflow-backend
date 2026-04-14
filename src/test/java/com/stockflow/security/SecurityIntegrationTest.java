da07184844427923f4dababb74213ddd3d9334d8
juan pablo cruz ulloa <cujp78@gmail.com>
2026-04-14 13:00:56 -0400

    feat: setup docker with postgres and spring boot

--- src/test/java/com/stockflow/security/SecurityIntegrationTest.java
+++ src/test/java/com/stockflow/security/SecurityIntegrationTest.java
@@ -16,6 +16,7 @@ import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMock
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.http.MediaType;
 import org.springframework.security.crypto.password.PasswordEncoder;
+import org.springframework.test.context.ActiveProfiles;
 import org.springframework.test.web.servlet.MockMvc;
 import org.springframework.test.web.servlet.MvcResult;
 
@@ -30,6 +31,7 @@ import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.
 
 @SpringBootTest
 @AutoConfigureMockMvc
+@ActiveProfiles("dev")
 public class SecurityIntegrationTest {
     @Autowired
     private MockMvc mockMvc;
