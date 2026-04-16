bf2accc01e7003fe8d5e0444bba13d924b8e253a
juan pablo cruz ulloa <cujp78@gmail.com>
2026-04-16 17:54:04 -0400

    chore: fix OpenApiConfig

--- src/main/java/com/stockflow/config/OpenApiConfig.java
+++ src/main/java/com/stockflow/config/OpenApiConfig.java
@@ -2,6 +2,8 @@ package com.stockflow.config;
 
 import io.swagger.v3.oas.models.OpenAPI;
 import io.swagger.v3.oas.models.info.Info;
+import io.swagger.v3.oas.models.security.SecurityRequirement;
+import io.swagger.v3.oas.models.security.SecurityScheme;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 
@@ -14,6 +16,16 @@ public class OpenApiConfig {
                 .info(new Info()
                         .title("StockFlow API")
                         .version("1.0")
-                        .description("Inventory and Order Management System"));
+                        .description("Inventory and Order Management System"))
+                .addSecurityItem(new SecurityRequirement().addList("bearerAuth"))
+
+                .components(new io.swagger.v3.oas.models.Components()
+                        .addSecuritySchemes("bearerAuth",
+                                new SecurityScheme()
+                                        .type(SecurityScheme.Type.HTTP)
+                                        .scheme("bearer")
+                                        .bearerFormat("JWT")
+                        )
+                );
     }
 }
